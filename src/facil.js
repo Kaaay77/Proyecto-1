8f6f7855b6fce14c748260cd5ae0af56de39debf
Kaaay77 <96114448+Kaaay77@users.noreply.github.com>
2022-06-15 14:14:18 +0200

    Update facil.js

--- src/facil.js
+++ src/facil.js
@@ -7,8 +7,8 @@ pelotaImage.src = "./src/images/pelotaPadel.png";
 let baseImage = new Image();
 baseImage.src = "./src/images/base.png";
 
-let base = new Base(250, canvas.height - 30, 200, 10, baseImage, 30, ctx);
-let pelota = new Pelota(0, 0, 20, 20, pelotaImage, 2, ctx);
+let base = new Base(200, canvas.height - 30, 200, 10, baseImage, 30, ctx);
+let pelota = new Pelota(0, 0, 30, 30, pelotaImage, 2, ctx);
 
 
 
@@ -47,15 +47,15 @@ const cargaInicial = () => {
   const incrementoVelocidadPelota =() =>{
 
     if (pelota.velocidad < 3){
-      pelota.velocidad += 2
+      pelota.velocidad = 4
     }
     
     console.log(pelota.velocidad);
 }
 
 
-    setInterval(incrementoVelocidadPelota, 20000);
-    setInterval(moverPelota, 00);
+    setInterval(incrementoVelocidadPelota, 10000);
+    setInterval(moverPelota, 01);
 };
 
 const logKey = (e) => {
